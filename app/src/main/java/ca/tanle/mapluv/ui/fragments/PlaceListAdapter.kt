4e0f79524d5b63c35e122eb906ab479cd358a456
tanle-dev <lenhuttan.programing@gmail.com>
2024-06-23 17:08:07 -0400

    Set up RC Adapter

---
+++ app/src/main/java/ca/tanle/mapluv/ui/fragments/PlaceListAdapter.kt
@@ -0,0 +1,24 @@
+package ca.tanle.mapluv.ui.fragments
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ca.tanle.mapluv.data.models.Place
+
+class PlaceListAdapter(val data: ArrayList<Place>): RecyclerView.Adapter<ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+}
