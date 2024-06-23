e6d44343b0f75604340202c5479f80e37b994d2b
tanle-dev <lenhuttanprograming@gmail.com>
2024-06-23 16:56:35 -0400

    init

--- app/src/main/java/ca/tanle/mapluv/ui/fragments/PlacesFragment.kt
+++ app/src/main/java/ca/tanle/mapluv/ui/fragments/PlacesFragment.kt
@@ -12,11 +12,6 @@ import ca.tanle.mapluv.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PlacesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlacesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
